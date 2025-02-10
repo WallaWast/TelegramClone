ec6d3d2b0c06f195407ce5f45ebb5ac37e2d75fd
Wallace Torres <Wallace.DaSilvaTorres@campus-support.ca>
2025-02-10 10:41:39 -0800

    Initial structure

---
+++ src/app/(home)/(tabs)/profile.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export default function MainTab() {
+	return <Text>Profile</Text>;
+}
---
+++ src/app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Slot } from 'expo-router';
+
+// define global providers
+export default function RootLayout() {
+	return <Slot />;
+}
